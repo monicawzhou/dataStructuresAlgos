a1bea10d5106b73a26c0c46056c7b8d2dfd2a187
Monica Zhou <mzhou.college@gmail.com>
2017-04-02 16:38:56 -0400

    ch4 Lafore textbook code

---
+++ chapter4/PriorityQApp.java
@@ -0,0 +1,24 @@
+package chapter4;
+
+import java.io.IOException;
+
+// listing 4.6
+class PriorityQApp
+{
+	public static void main(String[] args) throws IOException
+	{
+		PriorityQ thePQ = new PriorityQ(5);
+		thePQ.insert(30);
+		thePQ.insert(50);
+		thePQ.insert(10);
+		thePQ.insert(40);
+		thePQ.insert(20);
+		while( !thePQ.isEmpty() )
+		{
+			long item = thePQ.remove();
+			System.out.print(item + " "); // 10, 20, 30, 40, 50
+		} // end while
+		System.out.println("");
+	} // end main()
+	//-------------------------------------------------------------
+} // end class PriorityQApp
---
+++ chapter4/StackApp.java
@@ -0,0 +1,20 @@
+package chapter4;
+// listing 4.1
+class StackApp
+{
+	public static void main(String[] args)
+	{
+		StackX theStack = new StackX(10); // make new stack
+		theStack.push(20); // push items onto stack
+		theStack.push(40);
+		theStack.push(60);
+		theStack.push(80);
+		while( !theStack.isEmpty() ) // until it’s empty,
+		{ // delete item from stack
+			long value = theStack.pop();
+			System.out.print(value); // display it
+			System.out.print(" ");
+		} // end while
+		System.out.println("");
+	} // end main()
+} // end class StackApp
