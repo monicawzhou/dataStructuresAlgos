1527aaffd55443a5f07e40fe58d42c000af19bef
Monica Zhou <mzhou.college@gmail.com>
2017-04-07 21:36:57 -0400

    ch5 Lafore textbook code

---
+++ chapter5/Link.java
@@ -0,0 +1,12 @@
+package chapter5;
+class Link
+{
+	public long dData; // data item
+	public Link next; // next link in list
+	// -------------------------------------------------------------
+	public Link(long dd) // constructor
+	{ dData = dd; }
+	// -------------------------------------------------------------
+	public void displayLink() // display this link
+	{ System.out.print(dData + " "); }
+} // end class Link
